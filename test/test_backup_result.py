ee0bf1fa908c8446dff311421db10499571c4386
Niels Kamp <n.kamp@developers.nl>
2019-06-03 08:56:44 +0200

    Fixed a bug with non-tar files. DAT-85

--- test/test_backup_result.py
+++ test/test_backup_result.py
@@ -38,37 +38,33 @@ def get_temp_dir(temp_dir_location, dir_name):
     return temp_dir
 
 
-def get_project_name(google_file_name, suffix=True):
-    base_name = google_file_name.split("/")[-1]
-    if not suffix:
-        base_name = base_name.split(".")[0]
-    return base_name
-
-
 def test_backup_success(bucket, file, temp_location):
     try:
         tar_dir = get_temp_dir(temp_location, 'tar')
         bare_repo_dir = get_temp_dir(temp_location, 'repo')
         reconstructed_repo_dir = get_temp_dir(temp_location, 'work')
 
-        tar_location = '%s/%s' % (tar_dir, get_project_name(file))
+        base_tar_name = file.split("/")[-1]
+
+        tar_location = '%s/%s' % (tar_dir, base_tar_name)
 
         download_blob(bucket, file, tar_location)
+        if tar_location.endswith(".tar.bz2"):
 
-        tar = tarfile.open(tar_location, "r:bz2")
-        tar.extractall(path=bare_repo_dir)
-        tar.close()
+            tar = tarfile.open(tar_location, "r:bz2")
+            tar.extractall(path=bare_repo_dir)
+            tar.close()
 
-        git_directory = get_project_name(file, False) + ".git"
-        bare_repo_dir = '%s/%s' % (bare_repo_dir, git_directory)
+            git_directory = base_tar_name.split(".")[0] + ".git"
+            bare_repo_dir = '%s/%s' % (bare_repo_dir, git_directory)
 
-        cloned_repo = git.Repo.clone_from(bare_repo_dir, reconstructed_repo_dir)
+            cloned_repo = git.Repo.clone_from(bare_repo_dir, reconstructed_repo_dir)
 
-        assert os.path.isdir(cloned_repo.working_tree_dir)
-        assert os.listdir(cloned_repo.working_tree_dir)
+            assert os.path.isdir(cloned_repo.working_tree_dir)
+            assert os.listdir(cloned_repo.working_tree_dir)
 
-        file_string = ", ".join(os.listdir(cloned_repo.working_tree_dir))
+            file_string = ", ".join(os.listdir(cloned_repo.working_tree_dir))
 
-        logging.info('Found the following files and directories at root level: {}'.format(file_string))
+            logging.info('Found the following files and directories at root level: {}'.format(file_string))
     finally:
         shutil.rmtree(temp_location)
