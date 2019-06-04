1e00fd09909254678c9963a96e177d7832b06059
Niels Kamp <n.kamp@developers.nl>
2019-06-04 14:34:09 +0200

    Added Cloudbuild for bucket creation + Create Permission Script DAT-108

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ GOOGLE_STORAGE_BUCKET = "{}-backup".format(os.environ['PROJECT_ID'])
 # Base path of blobs stored
 GIT_BACKUP_BASE_PATH = 'git-backup'
 #Expected Location of data catalog.
-DATA_CATALOG = 'data_catalog.json'
+DATA_CATALOG = 'config/data_catalog.json'
---
+++ permissions/generate_permission_file.py
@@ -0,0 +1,14 @@
+import sys
+import json
+
+if len(sys.argv) > 1:
+    project_name = sys.argv[1]
+    data = {"bindings": []}
+
+    data["bindings"].append({
+        "role": "roles/storage.legacyBucketWriter",
+        "members": ["serviceAccount: {}@appspot.gserviceaccount.com".format(project_name)]
+    })
+
+    with open("{}.json".format(project_name), 'w') as output:
+        json.dump(data, output)
