c64a87333bf3d1578c24786bc0c5d7fe41b286db
Niels Kamp <n.kamp@developers.nl>
2019-06-05 13:33:06 +0200

    Removed etag. DAT-108

--- permissions/generate_permission_file.py
+++ permissions/generate_permission_file.py
@@ -1,12 +1,10 @@
-import base64
 import sys
 import json
 
 if len(sys.argv) > 1:
     project_name = sys.argv[1]
-    etag = base64.b64encode(project_name.encode('utf-8'))
 
-    data = {"bindings": [], "etag": str(etag)[2:-1]}
+    data = {"bindings": []}
 
     data["bindings"].append({
         "role": "roles/storage.legacyBucketWriter",
