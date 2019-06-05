180e7e1bd6b4629362b22be1f60256d035ef6e62
Niels Kamp <n.kamp@developers.nl>
2019-06-05 15:12:56 +0200

    Added additional bucket configuration. DAT-108

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Google Storage Bucket
-GOOGLE_STORAGE_BUCKET = "{}-backup".format(os.environ['PROJECT_ID'])
+GOOGLE_STORAGE_BUCKET = "{}-{}-backup-stg".format(os.environ['BACKUP_PROJECT_ID'], os.environ['PROJECT_ID'])
 # Base path of blobs stored
 GIT_BACKUP_BASE_PATH = 'git-backup'
 #Expected Location of data catalog.
